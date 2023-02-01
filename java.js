598f34934f8bc8100636428070e5b3bc9e7397b0
dariozombie <dariozombie666@gmail.com>
2023-02-01 13:00:20 -0300

    Se agraga proyecto

---
+++ java.js
@@ -0,0 +1,30 @@
+document.getElementById('btnradio1').addEventListener ('click',function(){
+
+document.getElementById('info').innerHTML="Dario Guillermo Garcia";
+
+});
+
+document.getElementById('btnradio2').addEventListener ('click',function(){
+
+document.getElementById('info').innerHTML="dariozombie666@gmail.com";
+
+});
+
+document.getElementById('btnradio3').addEventListener ('click',function(){
+
+document.getElementById('info').innerHTML="11/11/1982";
+
+});
+
+document.getElementById('btnradio4').addEventListener ('click',function(){
+
+document.getElementById('info').innerHTML="Vieytes 129";
+
+});
+
+document.getElementById('btnradio5').addEventListener ('click',function(){
+
+document.getElementById('info').innerHTML="11-38178095";
+
+});
+
